320e1c8aa61d917d6ad27d81e53f5517d923728e
WongYC-66 <yengchien@hotmail.com>
2024-07-18 15:13:12 +0800

    use createHashRouter

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import ErrorPage from './error/Error.jsx'
 import Layout from './layout/layout.jsx'
 
-const myRouter = createBrowserRouter([
+// const myRouter = createBrowserRouter([
+const myRouter = createHashRouter([
     {
-        path: "/odin-messaging-app-FE/",
+        // path: "/odin-messaging-app-FE/",
+        path: "/",
         element: <Layout />,
         errorElement: <ErrorPage />,
         children: [
             {
-                path: "contacts/:contactId",
+                path: "contacts",
                 element: <div>hi im contact </div>,
             },
         ]
