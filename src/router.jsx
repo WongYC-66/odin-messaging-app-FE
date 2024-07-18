17b8c277e92c7cdbc154ff93139b20a00b4c5bee
WongYC-66 <yengchien@hotmail.com>
2024-07-18 13:27:54 +0800

    add in react router

---
+++ src/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const myRouter = createBrowserRouter([
+    {
+        path: "/odin-messaging-app-FE/",
+        element: <div>Hello world!</div>,
+    },
+]);
+
+export default myRouter
