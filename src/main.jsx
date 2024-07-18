49f712df753e93eed9ef385d5073cf863f6b4792
WongYC-66 <yengchien@hotmail.com>
2024-07-18 13:59:02 +0800

    test router nested in gh

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { RouterProvider } from "react-router-dom";
 
 import myRouter from './router.jsx'
 
-console.log(myRouter)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={myRouter} />
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import ErrorPage from './error/Error.jsx'
+import Layout from './layout/layout.jsx'
+
 const myRouter = createBrowserRouter([
     {
         path: "/odin-messaging-app-FE/",
-        element: <div>Hello world!</div>,
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <div>hi im contact </div>,
+            },
+        ]
     },
+
 ]);
 
 export default myRouter
