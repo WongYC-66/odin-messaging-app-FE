5845717a60ee6429de48c3aa1224c4ee0c8449ca
WongYC-66 <yengchien@hotmail.com>
2024-08-30 21:44:43 +0800

    add visitor feature

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,51 @@ import { Form, Link, redirect, useActionData } from "react-router-dom";
 
 import API_URL from "../layout/API_URL.jsx"
 
+export default function SignIn() {
+
+    const actionData = useActionData();
+
+    return (
+        <div className="">
+            <h1 className="text-primary">Hang out </h1>
+            <h2 className="text-info">whenever,</h2>
+            <h2 className="text-primary-emphasis mb-5">wherever</h2>
+
+            <p className="text-primary-emphasis mb-3 w-50">MessageMe makes it easy to send a chat message to your friend or even a stranger! </p>
+
+            {actionData && actionData.error && <h5 className="text-danger">{actionData.error} </h5>}
+
+            <Form method="POST">
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="username" name='username' placeholder="" required />
+                    <label htmlFor="username">username</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="password" name="password" placeholder="" required />
+                    <label htmlFor="password">Password</label>
+                </div>
+
+                <button type="submit" className="btn btn-primary my-3">Log in</button>
+            </Form>
+
+            {/* Visitor Log-in Feature */}
+            <Form method="POST" className="d-inline-block">
+                <input type="text" name='username' value="visitor" hidden/>
+                <input type="password" name="password" value="visitor" hidden/>
+                <button type="submit" className="btn btn-primary my-3">or Log in as Visitor</button>
+            </Form>
+
+            <p className="mt-3">
+                <span className="my-1"> Doesn't have account ? </span>
+                <Link to={"../sign-up"} className="text-decoration-none">
+                    Sign Up here
+                </Link>
+
+            </p>
+        </div>
+    );
+}
+
 export async function action({ request }) {
 
     const formData = await request.formData();
@@ -26,51 +71,10 @@ export async function action({ request }) {
             token: data.token
         }))
         return redirect('/')
-    } 
+    }
 
     // Return the error data instead of redirecting, capturable at useActionData
     return {
         error: data.error || 'Unknown error occurred'
     };
-}
-
-export default function SignIn() {
-
-    const actionData = useActionData();
-
-
-    return (
-        <div className="">
-            <h1 className="text-primary">Hang out </h1>
-            <h2 className="text-info">whenever,</h2>
-            <h2 className="text-primary-emphasis mb-5">wherever</h2>
-
-            <p className="text-primary-emphasis mb-3 w-50">MessageMe makes it easy to send a chat message to your friend or even a stranger! </p>
-
-            {actionData && actionData.error && <h5 className="text-danger">{actionData.error} </h5>}
-
-
-            <Form method="POST">
-                <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="username" name='username' placeholder="" required />
-                    <label htmlFor="username">username</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" id="password" name="password" placeholder="" required />
-                    <label htmlFor="password">Password</label>
-                </div>
-
-                <button type="submit" className="btn btn-primary my-3">Log in</button>
-
-            </Form>
-
-            <p className="mt-3">
-                <span className="my-1"> Doesn't have account ? </span>
-                <Link to={"../sign-up"} className="text-decoration-none">
-                    Sign Up here
-                </Link>
-
-            </p>
-        </div>
-    );
 }
